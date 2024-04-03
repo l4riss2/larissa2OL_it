dee6ca1e621cf4a40584ec13e85b424180487a57
ana carla <>
2024-04-03 11:56:29 -0300

    attsite
    att site 03 04

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for(let card of cards){
     card.addEventListener("click",function(){
       const videoId =card.getAttribute("id");
       modal_overlay.classList.add('active')
-      modal_overlay.querySelector("iframe").src="https://www.youtube.com/embed/${videoId}";
+      modal_overlay.querySelector("iframe").src=`https://www.youtube.com/embed/${videoId}`;
     })
 }
 
